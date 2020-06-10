dddde0782f70beb8f3defeb7d57320c5a9791726
sergey zhiganov <id_zero@list.ru>
2020-06-10 16:44:14 +1000

    edit auth right

--- src/components/gapp.js
+++ src/components/gapp.js
@@ -61,7 +61,7 @@ class GApiAuth extends React.Component {
             scope: "https://www.googleapis.com/auth/documents.readonly " +
                 " https://www.googleapis.com/auth/drive.readonly" +
                 " https://www.googleapis.com/auth/drive" +
-                " https://www.googleapis.com/auth/drive.file"
+                " https://www.googleapis.com/auth/drive.file "
             ,
         }).then(_checkUserData);
     }
